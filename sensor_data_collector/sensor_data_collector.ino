ff288ad2392c7c8193a4e515fccd864050666bb7
Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>
2025-01-22 12:49:22 -0300

    Envia dados dos pulsos em json
    Talvez seja interessante enviar informações sobre o tempo em que o pulso
    foi coletado, então ta comentado ai.
    
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>

--- sensor_data_collector/sensor_data_collector.ino
+++ sensor_data_collector/sensor_data_collector.ino
@@ -1,31 +1,38 @@
+#define ARDUINOJSON_USE_LONG_LONG 1
+#include <ArduinoJson.h>
+
 const uint8_t sens[2] = { 2, 7 };
 bool midPulse[2] = { false, false };
 int lastValue[2];
 int currentValue[2];
 unsigned long timeStart[2];
 unsigned long diff[2];
+const char* bikeNames[2] = { "bike0", "bike1" };
 
 void setup() {
-  const long baud = 115200;
+  const long baud = 115200L;
 
   Serial.begin(baud);
   pinMode(sens[0], INPUT);
   pinMode(sens[1], INPUT);
   establishContact();
 
-  delay(1000);
+  delay(1000UL);
 
   lastValue[0] = digitalRead(sens[0]);
   lastValue[1] = digitalRead(sens[1]);
-  Serial.print("Started: ");
+  Serial.print("\nStarted: ");
   Serial.print(lastValue[0]);
   Serial.print("; ");
   Serial.println(lastValue[1]);
 }
 
 void loop() {
-  const int minPulse = 1000;
+  const char* currentTimeKey = "current_time";
+  const unsigned long minPulse = 10000UL;
   unsigned long currentTime;
+  StaticJsonDocument<12> doc[2];
+  // StaticJsonDocument<24> doc[2];
 
   currentValue[0] = digitalRead(sens[0]);
   currentValue[1] = digitalRead(sens[1]);
@@ -39,10 +46,9 @@ void loop() {
       } else {
         diff[i] = currentTime - timeStart[i];
         if (diff[i] >= minPulse) {
-          Serial.print("bike");
-          Serial.print(i);
-          Serial.print(": ");
-          Serial.println(diff[i]);
+          // doc[i][currentTimeKey] = currentTime;
+          doc[i][bikeNames[i]] = diff[i];
+          serializeJson(doc[i], Serial);
         }
       }
       lastValue[i] = currentValue[i];
@@ -55,7 +61,7 @@ void establishContact() {
 
   while (Serial.available() <= 0 || Serial.read() != 65) {
     Serial.print('A');  // send a capital A
-    delay(300);
+    delay(300UL);
   }
 
   Serial.flush();
